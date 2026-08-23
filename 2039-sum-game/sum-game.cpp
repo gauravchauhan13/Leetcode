f3cbed61d0580a2b4f1df298db8d80e6ae008176
Gaurav <gauravchauhan2318@gmail.com>
2026-08-23 19:59:47 +0530

    Time: 1 ms (77.62%) | Memory: 14.2 MB (5.36%) - LeetSync

---
+++ 2039-sum-game/sum-game.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+
+        int n = num.size();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        // First half
+        for (int i = 0; i < half; i++)
+        {
+            if (num[i] == '?')
+            {
+                leftQ++;
+            }
+            else
+            {
+                leftSum += num[i] - '0';
+            }
+        }
+
+        // Second half
+        for (int i = half; i < n; i++)
+        {
+            if (num[i] == '?')
+            {
+                rightQ++;
+            }
+            else
+            {
+                rightSum += num[i] - '0';
+            }
+        }
+
+        // Odd number of '?' means Alice gets the last move
+        if ((leftQ + rightQ) % 2 == 1)
+        {
+            return true;
+        }
+
+        int diff = leftSum - rightSum;
+
+        int required = 9 * (rightQ - leftQ) / 2;
+
+        return diff != required;
+    }
+};
